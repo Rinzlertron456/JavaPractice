f99484818e1f232e2eeb784512153fa8bd6ffcdd
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2024-05-23 11:02:54 +0530

    pushsort

--- src/LeadersArray.java
+++ src/LeadersArray.java
@@ -2,22 +2,28 @@ import java.util.ArrayList;
 
 public class LeadersArray {
     public static void main(String[] args) {
-        int[] A = new int[]{1, 2, 3, 4, 0};
+        int[] arr = new int[]{1, 2, 3, 4, 0};
         int n = 6;
         ArrayList<Integer> list = new ArrayList<>();
+        list=leaderlist(arr,list);
+        System.out.println(list);
+    }
 
-        for (int i = 0; i < A.length; ++i) {
-            boolean leader = true;
-            for (int j = i + 1; j < A.length; ++j) {
-                if (A[i] < A[j]) {
-                    leader = false;
-                }
-            }
+    public static ArrayList<Integer> leaderlist(int[] arr, ArrayList<Integer> list){
+        for (int i = 0; i < arr.length; i++) {
+            boolean leader=leader(i,arr);
+            if(leader) list.add(arr[i]);
+        }
+        return list;
+    }
 
-            if (leader) {
-                list.add(A[i]);
+    public static boolean leader(int i, int[] arr){
+        boolean leader = true;
+        for (int j = i + 1; j < arr.length; ++j) {
+            if (arr[i] < arr[j]) {
+                leader = false;
             }
         }
-        System.out.println(list);
+        return leader;
     }
 }
