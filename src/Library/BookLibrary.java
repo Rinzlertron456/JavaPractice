bc9b07b4281c664fc08e4a05e698c4b998b28c71
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2024-09-01 19:23:26 +0530

    pushsort

--- src/Library/BookLibrary.java
+++ src/Library/BookLibrary.java
@@ -3,9 +3,29 @@ package Library;
 import java.util.ArrayList;
 
 public class BookLibrary {
-
+    private static ArrayList<Book> books;
+    public BookLibrary() {
+        books=new ArrayList<>();
+    }
     public static void addBook(Book book){
-
+        books.add(book);
     }
 
+    public static ArrayList<Book> getBooks(){
+        return books;
+    }
+    public static ArrayList<String> getBooksByAuthor(String author){
+        ArrayList<String> authorbooks=new ArrayList<>();
+        for (Book book: books){
+            if(book.getAuthor().equals(author)) authorbooks.add(book.getTitle());
+        }
+        return authorbooks;
+    }
+    public static Double getTotalValue(){
+        Double TotalValue=0.0d;
+        for (Book book:books){
+            TotalValue+=book.getPrice();
+        }
+        return TotalValue;
+    }
 }
--- src/Library/LibraryManagementSystem.java
+++ src/Library/LibraryManagementSystem.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class LibraryManagementSystem {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Book book=new Book("1984","Orwell",299.99);
-        BookLibrary.addBook(book);
-//        int n=sc.nextInt();
-//        for (int i = 0; i < n; i++) {
-//            String title=sc.next();
-//            String author=sc.next();
-//            Double price=sc.nextDouble();
-//            Book book=new Book(title,author,price);
-//
-//        }
-        System.out.println();
-        System.out.println();
+        Book book1=new Book("1984","Orwell",299.99);
+        Book book2=new Book("The_Great_Gatsby_F.", "Scott_Fitzgerald", 10.99);
+        Book book3=new Book ("To_Kill_a_Mockingbird", "Harper_Lee", 7.99);
+        BookLibrary bl=new BookLibrary();
+        bl.addBook(book1);
+        bl.addBook(book2);
+        bl.addBook(book3);
+        String author=sc.next();
+        System.out.println(bl.getBooksByAuthor(author));
+        System.out.println(bl.getTotalValue());
     }
 }
 
