9498b705f97b72cdd9966357ca1e3ef0af9972a8
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2025-01-09 16:04:22 +0530

    push-new-leetcode-reverse-vowel-and-string-compression

--- src/ReverseVowelString.java
+++ src/ReverseVowelString.java
@@ -3,26 +3,61 @@ import java.util.HashMap;
 
 public class ReverseVowelString {
     public static void main(String[] args) {
-        String s = "IceCreAm";
+        String s="c#dc";
+        //Optimised Approach
+        String vowels = "aeiouAEIOU";
+        boolean alpha = true;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(!((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))) alpha=false;
+            else alpha=true;
+        }
+        if(alpha==false) System.out.println(s);
         char[] letters = s.toCharArray();
-        ArrayList<Character> vowels= new ArrayList<>();
-        for (int i = 0; i < letters.length; i++) {
-            char letter = letters[i];
-            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'
-                    || letter == 'A' ||letter == 'E' ||letter == 'I' ||letter == 'O' ||letter == 'U') {
-                vowels.add(letters[i]);
-            }
+        int left = 0, right = s.length()-1;
+        if(s.length()==2&&vowels.contains(letters[left]+"")&&vowels.contains(letters[right]+"")) {
+            char temp = '\0';
+            temp = letters[left];
+            letters[left] = letters[right];
+            letters[right] = temp;
         }
-        int k = vowels.size()-1;
-        for (int i = 0; i < letters.length; i++) {
-            char letter = letters[i];
-            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'
-                    || letter == 'A' ||letter == 'E' ||letter == 'I' ||letter == 'O' ||letter == 'U') {
-                letters[i] = vowels.get(k);
-                k--;
+        while(left<(s.length()-1)/2){
+            while(!vowels.contains(letters[left]+"")) left++;
+            while(!vowels.contains(letters[right]+"")) right--;
+            if(vowels.contains(letters[left]+"")&&vowels.contains(letters[right]+"")){
+                char temp = '\0';
+                temp = letters[left];
+                letters[left] = letters[right];
+                letters[right] = temp;
             }
+            System.out.println("left: "+left);
+            System.out.println("right: "+right);
+            System.out.println("Left element: "+letters[left]);
+            System.out.println("Right Element: "+letters[right]);
+            if(left<s.length()) left++;
+            if(right<s.length())right--;
         }
-
+        //Conventional Approach
+//        ArrayList<Character> vowels= new ArrayList<>();
+//        char[] letters = s.toCharArray();
+//        int left = 0, right = s.length()-1;
+//        for (int i = 0; i < letters.length; i++) {
+//            char letter = letters[i];
+//            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'
+//                    || letter == 'A' ||letter == 'E' ||letter == 'I' ||letter == 'O' ||letter == 'U') {
+//                vowels.add(letters[i]);
+//            }
+//        }
+//        int k = vowels.size()-1;
+//        for (int i = 0; i < letters.length; i++) {
+//            char letter = letters[i];
+//            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'
+//                    || letter == 'A' ||letter == 'E' ||letter == 'I' ||letter == 'O' ||letter == 'U') {
+//                letters[i] = vowels.get(k);
+//                k--;
+//            }
+//        }
+//
         for (int i = 0; i < letters.length; i++) {
             System.out.println(letters[i]);
         }
