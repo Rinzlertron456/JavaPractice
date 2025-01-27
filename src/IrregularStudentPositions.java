3cf71e9a11a45eff3e3bb92234cf5c2c433eb88b
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-01-27 11:52:32 +0530

    push-new-leetcode-reverse-vowel-and-string-compression

--- src/IrregularStudentPositions.java
+++ src/IrregularStudentPositions.java
@@ -3,6 +3,20 @@ import java.util.*;
 public class IrregularStudentPositions {
     public static void main(String[] args) {
         List<Integer> height = new ArrayList<>();
-
+        height.add(1);
+        height.add(5);
+        height.add(3);
+        height.add(2);
+        height.add(1);
+        int[] arr = new int[height.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = height.get(i);
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(height.get(i)!=arr[i]) count++;
+        }
+        System.out.println(count);
     }
 }
---
+++ src/LexiSmallSeq.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class LexiSmallSeq {
+    public static void main(String[] args) {
+        List<Integer> priority = new ArrayList<>();
+        priority.add(2);
+        priority.add(4);
+        priority.add(6);
+        priority.add(4);
+        priority.add(3);
+        priority.add(2);
+        int[] arr = new int[priority.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = priority.get(i);
+        }
+        Collections.sort(priority);
+        int first = priority.get(0), second = priority.get(1);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j <= arr.length-1; j++) {
+                int temp = arr[i];
+                if(arr[i]>arr[j]) {
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.println("Iteration "+i);
+            for (int k = 0; k < arr.length; k++) {
+                System.out.print(arr[k]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
