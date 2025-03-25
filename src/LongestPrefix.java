2e274bff8110d23a1b523a0784e6d7db8973bb7b
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2025-03-25 09:25:07 +0530

    Javascript DSA Coding practice in Java - 05-03-2025

--- src/LongestPrefix.java
+++ src/LongestPrefix.java
@@ -1,38 +1,54 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LongestPrefix {
     public static void main(String[] args) {
-        String[] words={"dmlrpjyatcoqotxzplqmlptaipczhlikztcofaoaedruyqundkzqatqkkvjrgucineyugnxmsohsgdfmngcpbvamqldyfhgvnfrv", "oioerglunzjvbzxwblooqnuytrnyijuxtibkoogdppzrqyptjeizrezmvnnfyherqidgyjkoyjfrhwkscsrvytivivbgcfxupab", "llclwjcdfpvijodijndriexnmwhbyiplvtxrcbwkqtsaixitn", "lvskkgjujheztaustxtqhklbkvyupnhajbmvhvprfusawmspjlhsvtthouddhlfsmsqwpfpubhuzvmrhaazx"};
-        String comp="",prefix="";
-        //Solution 1
-        int min=Integer.MAX_VALUE;
-        for (int i = 0; i < words.length; i++) {
-            min=Math.min(min,words[i].length());
-        }
-        for (int i = 0; i < words.length; i++) {
-            if(words[i].length()==min) comp=words[i];
-        }
-        boolean check=false;
-//        ArrayList<String> compwords=new ArrayList<>();
+        // Temporary jugaad
+//        String[] words={"dmlrpjyatcoqotxzplqmlptaipczhlikztcofaoaedruyqundkzqatqkkvjrgucineyugnxmsohsgdfmngcpbvamqldyfhgvnfrv", "oioerglunzjvbzxwblooqnuytrnyijuxtibkoogdppzrqyptjeizrezmvnnfyherqidgyjkoyjfrhwkscsrvytivivbgcfxupab", "llclwjcdfpvijodijndriexnmwhbyiplvtxrcbwkqtsaixitn", "lvskkgjujheztaustxtqhklbkvyupnhajbmvhvprfusawmspjlhsvtthouddhlfsmsqwpfpubhuzvmrhaazx"};
+//        String comp="",prefix="";
+//        //Solution 1
+//        int min=Integer.MAX_VALUE;
 //        for (int i = 0; i < words.length; i++) {
-//            if(!(words[i].equals(prefix))) compwords.add(words[i]);
+//            min=Math.min(min,words[i].length());
 //        }
-        while(!check){
-            int k=0;
-            for (String word:words) {
-                for (int i=0;i<word.length();i++) {
-                    if(!((word.charAt(i)+"")==(comp.charAt(k)+""))){
-                        System.out.println("-1");
-                        check=false;
-                    }
-                    else {
-                            check=true;
-                            prefix+=comp.charAt(k);
-                        }
-                    k++;
-                }
-            }
+//        for (int i = 0; i < words.length; i++) {
+//            if(words[i].length()==min) comp=words[i];
+//        }
+//        boolean check=false;
+////        ArrayList<String> compwords=new ArrayList<>();
+////        for (int i = 0; i < words.length; i++) {
+////            if(!(words[i].equals(prefix))) compwords.add(words[i]);
+////        }
+//        while(!check){
+//            int k=0;
+//            for (String word:words) {
+//                for (int i=0;i<word.length();i++) {
+//                    if(!((word.charAt(i)+"")==(comp.charAt(k)+""))){
+//                        System.out.println("-1");
+//                        check=false;
+//                    }
+//                    else {
+//                            check=true;
+//                            prefix+=comp.charAt(k);
+//                        }
+//                    k++;
+//                }
+//            }
+//        }
+//        System.out.println(prefix);
+        String[] strs = {"flower", "flow", "flight"};
+        String str = "Hello World";
+        // Bruteforce Solution
+        Arrays.sort(strs);
+        String prefix = "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            if(strs[0].charAt(i)==strs[strs.length-1].charAt(i)) prefix = prefix + strs[0].charAt(i);
+            else break;
         }
+        //Optimised solution (In Progress )
+//        String prefix = "";
+//        Arrays.stream(strs).sorted();
         System.out.println(prefix);
     }
 }
